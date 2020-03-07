11cde4a77e50df2344e255b40e6109fd4d46b2a8
Josh Ellis <joshe523@gmail.com>
2020-03-07 17:38:20 -0600

    refactor w/ instance variables instead of passing variables through args

--- lib/cli.rb
+++ lib/cli.rb
@@ -18,11 +18,21 @@ class CommandLineInterface
     return
   end
 
-  # Displays list of games between @start_rank and @end_rank
-  def list_games(invalid_input=false)
+  def separator
     puts 
+    puts "• • •"
+    puts
+  end
+
+  # Displays list of games between @start_rank and @end_rank
+  def list_games
+
+    # Print separator
+    separator
+
+    # Print list header
     puts " #  | year | title"
-    puts "--------------------------------------"
+    puts "------------------------"
 
     # Prints each game: 2. | 2020 | Game name
     Game.all.select{|game| game.rank.to_i <= @end_rank && game.rank.to_i >= @start_rank}.each do |game| 
@@ -38,102 +48,154 @@ class CommandLineInterface
     puts "→ Enter 0 to see the next 10 titles"
     puts "→ Enter Q to quit"
 
-    # If method is called with invalid_input, display error message
-    puts "ERROR: Invalid input, please try again" if invalid_input == true
+    # If invalid_input, display error message and reset @invalid_input
+    if @invalid_input == true
+      puts "ERROR: Invalid input, please try again" 
+      @invalid_input = false
+    end
 
     # Get user input
-    input = gets.chomp
+    @input = gets.chomp
 
     # Parse user input
     
-    if input == "0"
+    if @input == "0"
       # If user inputs 0 for the next part of the list...
       if @end_rank == 50
         # ...if at the end of the list (50), loop to beginning
         @start_rank = 1
         @end_rank = 10
-        list_games(false) 
+        list_games
       else
         # ...otherwise, add 10 to @start_rank and @end_rank
         # Then call this method again (with new values)
         @start_rank += 10
         @end_rank += 10
-        list_games(false) 
+        list_games
       end
-    elsif input.to_i <= @end_rank && input.to_i >= @start_rank
+    elsif @input.to_i <= @end_rank && @input.to_i >= @start_rank
       # If user chooses a number between @start_rank - @end_rank, call the details method
-      game_details(input)
-    elsif input.downcase == 'q'
+      game_details
+    elsif @input.downcase == 'q'
       # If they quit, run "goodbye" method
       goodbye
     else
       # If input doesn't work, run method again with error message.
-      list_games(true) 
+      @invalid_input = true
+      list_games
     end
   end
 
   # Displays details of a chosen game
-  def game_details(rank,invalid_input=false)
+  def game_details
 
     # Variable for indentation string to make it easy to change
-    indent = "· " 
+    @indent = "· " 
 
     # Find the game that matches the selected rank
-    game = Game.all.select{|game| game.rank == rank}.first 
+    @game = Game.all.select{|game| game.rank == @input}.first 
     
     # Check to see if details exist, and scrapes if needed
-    game.get_details 
+    @game.get_details 
     
     # Print details
-    puts 
+
+    # Print separator
+    separator
 
     # 2. Game Name (2020)
-    puts "#{game.rank}. #{game.name} (#{game.year})"
+    puts "#{@game.rank}. #{@game.name} (#{@game.year})"
 
     # 2-4 players • 60-90 minutes • ages 12+
-    puts "#{game.minplayers}–#{game.maxplayers} players • #{game.minplaytime}–#{game.maxplaytime} minutes • ages #{game.minage}+"
+    puts "#{@game.minplayers}–#{@game.maxplayers} players • #{@game.minplaytime}–#{@game.maxplaytime} minutes • ages #{@game.minage}+"
 
     # https://boardgamegeek.com/boardgame/ID
-    puts "#{game.url}"
+    puts "#{@game.url}"
     puts
 
     # Use wrap method to add indentation & word wrap
     puts "DESCRIPTION:"
-    puts wrap("#{game.description[0..180]}...",indent) 
+    puts wrap("#{@game.description[0..140]}...",@indent) 
     puts
 
     # Use print_array method to print arrays (with wrap method)
-    print_array("categories", "category", indent, game.category) 
-    print_array("mechanics", "mechanic", indent, game.mechanic)
-    print_array("publishers", "publisher", indent, game.publisher)
-    print_array("designers", "designer", indent, game.designer)
+    print_array("categories", "category", @game.category) 
+    print_array("mechanics", "mechanic", @game.mechanic)
     puts
 
+    # See what user wants to do next
+    details_input
+  end
+
+  # Displays full description
+  def full_description
+
+    # Print separator
+    separator
+
+    # Use wrap method to add indentation & word wrap
+    puts "DESCRIPTION:"
+    puts wrap("#{@game.description}",@indent) 
+    puts 
+
+    # See what user wants to do next
+    details_input
+  end
+
+  # Displays publisher(s) and designer(s)
+  def publisher_designer
+    
+    # Print separator
+    separator
+
+    # Use print_array method to print arrays (with wrap method)
+    print_array("publishers", "publisher", @game.publisher)
+    print_array("designers", "designer", @game.designer)
+    puts 
+
+    # See what user wants to do next
+    details_input
+  end
+
+  # Input loop when in @game's details
+  def details_input
     # Prompt user input
     puts "→ Enter 0 to return to the list"
+    puts "→ Enter 1 to see full description"
+    puts "→ Enter 2 to see publisher & designer"
     puts "→ Enter Q to quit"
 
-    # If method is called with invalid_input, display error message
-    puts "ERROR: Invalid input, please try again" if invalid_input == true
+    # If @invalid_input is true, display error message & reset @invalid_input
+    if @invalid_input == true
+      puts "ERROR: Invalid input, please try again" 
+      @invalid_input = false
+    end
 
     # Get user input
-    input = gets.chomp
+    @input = gets.chomp
 
     # Parse user input
-    if input == "0" 
+    if @input == "0" 
       # If they choose 0, return to the list
-      list_games(false) 
-    elsif input.downcase == "q" 
+      list_games
+    elsif @input == "1"
+      # If they choose 1, print full description
+      full_description
+    elsif @input == "2"
+      # If they choose 2, print publisher and designer
+      publisher_designer
+    elsif @input.downcase == "q" 
       # If they quit, run "goodbye" method
       goodbye
     else
       # If input doesn't work, run method again with error message.
-      game_details(rank,true) 
+      @invalid_input = true
+      game_details
     end
   end
 
   # Prints array with commas as needed
-  def print_array(plural, single, indent, array)
+  def print_array(plural, single, array)
   # Sometimes new games have empty fields.
   # Don't do anything if array is empty.
   if array.size != 0 
@@ -159,7 +221,7 @@ class CommandLineInterface
       end
 
       # print the output, truncate if too long
-      puts output.size < 120 ? wrap(output, indent) : wrap("#{output[0..120]}...",indent)
+      puts output.size < 120 ? wrap(output, @indent) : wrap("#{output[0..120]}...",@indent)
     end
   end
 
@@ -170,8 +232,10 @@ class CommandLineInterface
     Scraper.new("https://www.boardgamegeek.com/xmlapi2/hot?boardgame").game_list
     @start_rank = 1
     @end_rank = 10
+    @invalid_input = false
+    @input = ""
     welcome
-    list_games(false)
+    list_games
   end
 
   # NOTE: #run must be last so other methods can load.
