186fa2a7700a601519991f432102ab408c18b121
Josh Ellis <joshe523@gmail.com>
2020-03-08 11:47:46 -0500

    formatting

--- lib/bgg-hotness-cli/helpers.rb
+++ lib/bgg-hotness-cli/helpers.rb
@@ -5,33 +5,33 @@ def wrap(s, indent, width=78)
 end
 
 # Prints array with commas as needed
-  def print_array(plural, single, array, indent)
-  # Sometimes new games have empty fields.
-  # Don't do anything if array is empty.
-  if array.size != 0 
-    puts array.size > 1 ? "#{plural.upcase}: " : "#{single.upcase}: "
-    
-    # Initialize variable for holding output string
-    output = "" 
-      array.each_with_index do |item,idx| 
-        output += item
+def print_array(plural, single, array, indent)
+# Sometimes new games have empty fields.
+# Don't do anything if array is empty.
+if array.size != 0 
+  puts array.size > 1 ? "#{plural.upcase}: " : "#{single.upcase}: "
+  
+  # Initialize variable for holding output string
+  output = "" 
+    array.each_with_index do |item,idx| 
+      output += item
 
-        # if there's more than one item and this isn't the last item, add commas
-        if item != array.last && array.size > 1
+      # if there's more than one item and this isn't the last item, add commas
+      if item != array.last && array.size > 1
 
-          # print an & before last item
-          if idx == array.size - 2 
-            output += ", & " 
+        # print an & before last item
+        if idx == array.size - 2 
+          output += ", & " 
 
-          # otherwise, just print a comma and space
-          else 
-            output += ", " 
-          end
+        # otherwise, just print a comma and space
+        else 
+          output += ", " 
         end
       end
-
-      # print the output with word-wrapping
-      puts wrap(output, indent)
-      puts
     end
+
+    # print the output with word-wrapping
+    puts wrap(output, indent)
+    puts
   end
+end
